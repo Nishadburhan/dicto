a3a553c31cab1614f9890f14559d0b513ea61550
nishadburhan <nishadburhanofficial@gmail.com>
2018-10-24 12:01:20 +0530

    host updated

--- app.js
+++ app.js
@@ -13,28 +13,28 @@ const port = 8000;
 let gAddress;
 Object.keys(ifaces).forEach(function (ifname) {
     var alias = 0;
-  
+
     ifaces[ifname].forEach(function (iface) {
-      if ('IPv4' !== iface.family || iface.internal !== false) {
-        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-        return;
-      }
-    gAddress=iface.address;
-      
-    //   console.log(iface.address);
-      
-    //   if (alias >= 1) {
-    //     // this single interface has multiple ipv4 addresses
-    //     console.log(ifname + ':' + alias, iface.address);
-    //     port=iface.address;
-    //   } else {
-    //     // this interface has only one ipv4 adress
-    //     console.log(ifname, iface.address);
-    //     port=iface.address;
-    //   }
-    //   ++alias;
+        if ('IPv4' !== iface.family || iface.internal !== false) {
+            // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+            return;
+        }
+        gAddress = iface.address;
+
+        //   console.log(iface.address);
+
+        //   if (alias >= 1) {
+        //     // this single interface has multiple ipv4 addresses
+        //     console.log(ifname + ':' + alias, iface.address);
+        //     port=iface.address;
+        //   } else {
+        //     // this interface has only one ipv4 adress
+        //     console.log(ifname, iface.address);
+        //     port=iface.address;
+        //   }
+        //   ++alias;
     });
-  });
+});
 
 // Connect to database
 mongoose.connect(config.database, { useNewUrlParser: true });
@@ -79,6 +79,6 @@ app.get('/', (req, res) => {
 });
 
 // Init Server
-app.listen(port , gAddress ,() => {
+app.listen(port, () => {
     console.log(`Server started at ${port}`);
 });
